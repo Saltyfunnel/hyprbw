eac863dfdc5347a6edaf0e81e3a7ccf8be1df038
Saltyfunnel <gerardsheal@gmail.com>
2025-07-17 12:42:30 +0100

    Update utilities.sh

--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -19,13 +19,33 @@ REPO_DIR="$USER_HOME/hyprbw"
 ASSETS_SRC="$REPO_DIR/assets"
 ASSETS_DEST="$CONFIG_DIR/assets"
 
+# Function to safely copy directory as the user
+copy_as_user() {
+    local src="$1"
+    local dest="$2"
+
+    if [ ! -d "$src" ]; then
+        print_warning "Source folder not found: $src"
+        return 1
+    fi
+
+    # Make sure destination exists
+    run_command "mkdir -p \"$dest\"" "Create destination directory $dest" "no" "no"
+
+    # Copy content as the user
+    run_command "cp -r \"$src\"/* \"$dest\"" "Copy from $src to $dest" "yes" "no"
+
+    # Fix ownership recursively to the user
+    run_command "chown -R $SUDO_USER:$SUDO_USER \"$dest\"" "Fix ownership for $dest" "no" "yes"
+}
+
 # Waybar
 run_command "pacman -S --noconfirm waybar" "Install Waybar - Status Bar" "yes"
-run_command "cp -r $REPO_DIR/configs/waybar $CONFIG_DIR/" "Copy Waybar config" "yes" "no"
+copy_as_user "$REPO_DIR/configs/waybar" "$CONFIG_DIR/waybar"
 
 # Tofi
 run_command "yay -S --sudoloop --noconfirm tofi" "Install Tofi - Application Launcher" "yes" "no"
-run_command "cp -r $REPO_DIR/configs/tofi $CONFIG_DIR/" "Copy Tofi config(s)" "yes" "no"
+copy_as_user "$REPO_DIR/configs/tofi" "$CONFIG_DIR/tofi"
 
 # Cliphist
 run_command "pacman -S --noconfirm cliphist" "Install Cliphist - Clipboard Manager" "yes"
@@ -33,28 +53,21 @@ run_command "pacman -S --noconfirm cliphist" "Install Cliphist - Clipboard Manag
 # SWWW
 run_command "yay -S --sudoloop --noconfirm swww" "Install SWWW for wallpaper management" "yes" "no"
 
-# Backgrounds (ensure target dir & copy as user)
-run_command "mkdir -p $ASSETS_DEST/backgrounds" "Create backgrounds directory" "no" "no"
-if [ -d "$ASSETS_SRC/backgrounds" ]; then
-    run_command "cp -r $ASSETS_SRC/backgrounds/* $ASSETS_DEST/backgrounds/" "Copy background images" "yes" "no"
-else
-    print_warning "Backgrounds folder not found at $ASSETS_SRC/backgrounds"
-    log_message "Backgrounds not copied: missing folder."
-fi
+# Backgrounds (copy assets/backgrounds)
+copy_as_user "$ASSETS_SRC/backgrounds" "$ASSETS_DEST/backgrounds"
 
 # Hyprpicker
 run_command "yay -S --sudoloop --noconfirm hyprpicker" "Install Hyprpicker - Color Picker" "yes" "no"
 
 # Hyprlock
 run_command "yay -S --sudoloop --noconfirm hyprlock" "Install Hyprlock - Screen Locker" "yes" "no"
-run_command "mkdir -p $CONFIG_DIR/hypr" "Ensure Hypr config dir exists" "no" "no"
-run_command "cp -r $REPO_DIR/configs/hypr/hyprlock.conf $CONFIG_DIR/hypr/" "Copy Hyprlock config" "yes" "no"
+copy_as_user "$REPO_DIR/configs/hypr" "$CONFIG_DIR/hypr"
 
 # Grimblast
 run_command "yay -S --sudoloop --noconfirm grimblast" "Install Grimblast - Screenshot tool" "yes" "no"
 
 # Hypridle
 run_command "yay -S --sudoloop --noconfirm hypridle" "Install Hypridle for idle management" "yes" "no"
-run_command "cp -r $REPO_DIR/configs/hypr/hypridle.conf $CONFIG_DIR/hypr/" "Copy Hypridle config" "yes" "no"
+# hypridle config copied above as part of hypr folder
 
 echo "------------------------------------------------------------------------"
